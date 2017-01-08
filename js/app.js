618b2d382ae6dd326a653eda7e271d9e9b0c1f86
gnarfugh <kmorales076@gmail.com>
2017-01-08 17:24:47 -0600

    menu + slide

--- js/app.js
+++ js/app.js
@@ -63,95 +63,10 @@ $(document).ready(function() {
         });
     };
 
-    $(function() {
-
-        // Get the form.
-        var form = $('#ajax-contact');
-
-        // Get the messages div.
-        var formMessages = $('#form-messages');
-
-        // Set up an event listener for the contact form.
-        $(form).submit(function(e) {
-            // Stop the browser from submitting the form.
-            e.preventDefault();
-
-            // Serialize the form data.
-            var formData = $(form).serialize();
-
-            // Submit the form using AJAX.
-            $.ajax({
-                    type: 'POST',
-                    url: $(form).attr('action'),
-                    data: formData
-                })
-                .done(function(response) {
-                    // Make sure that the formMessages div has the 'success' class.
-                    $(formMessages).removeClass('error');
-                    $(formMessages).addClass('success');
-
-                    // Set the message text.
-                    $(formMessages).text(response);
-
-                    // Clear the form.
-                    $('#name').val('');
-                    $('#email').val('');
-                    $('#message').val('');
-                })
-                .fail(function(data) {
-                    // Make sure that the formMessages div has the 'error' class.
-                    $(formMessages).removeClass('success');
-                    $(formMessages).addClass('error');
-
-                    // Set the message text.
-                    if (data.responseText !== '') {
-                        $(formMessages).text(data.responseText);
-                    } else {
-                        $(formMessages).text('Oops! An error occured and your message could not be sent.');
-                    }
-                });
-
-        });
-
-    });
-
     // initialize wow.js
     new WOW().init();
 
-    // Push the body and the nav over by 285px over
-    var main = function() {
-        $('.fa-bars').click(function() {
-            $('.nav-screen').animate({
-                right: "0px"
-            }, 200);
-
-            $('body').animate({
-                right: "300px"
-            }, 200);
-        });
-
-        // Then push them back */
-        $('.fa-times').click(function() {
-            $('.nav-screen').animate({
-                right: "-300px"
-            }, 200);
-
-            $('body').animate({
-                right: "0px"
-            }, 200);
-        });
-
-        $('.nav-links a').click(function() {
-            $('.nav-screen').animate({
-                right: "-300px"
-            }, 500);
-
-            $('body').animate({
-                right: "0px"
-            }, 500);
-        });
-    };
-
+    //slider
     $('#slider').slick({
         dots: true,
         infinite: true,
@@ -163,44 +78,55 @@ $(document).ready(function() {
         slidesToScroll: 4,
     });
 
-    // Page Scroll
-    $('a[href*=#]:not([href=#])').click(function() {
-        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') || location.hostname == this.hostname) {
+    //FAQ
+    $(".faq-q").click(function(e) {
+        var container = $(this).parents(".faq-c");
+        var answer = container.find(".faq-a");
+        var trigger = container.find(".faq-t");
 
-            var target = $(this.hash);
-            target = target.length ? target : $('[name=' + this.hash.slice(1) + ']');
-            if (target.length) {
-                $('html,body').animate({
-                    scrollTop: target.offset().top - 32
-                }, 1000);
-                return false;
-            }
+        answer.slideToggle(200);
+
+        if (trigger.hasClass("faq-o")) {
+            trigger.removeClass("faq-o");
+        } else {
+            trigger.addClass("faq-o");
         }
+        e.preventDefault();
     });
 
-    // Fixed Nav
-    $(window).scroll(function() {
-        var scrollTop = 366;
-        if ($(window).scrollTop() >= scrollTop) {
-            $('nav').css({
-                position: 'fixed',
-                top: '0'
-            });
-            $('nav ul').css({
-                display: 'flex'
-            })
-        }
-        if ($(window).scrollTop() < scrollTop) {
-            $('nav').removeAttr('style');
-            $('nav ul').removeAttr('style');
-        }
+    //nav mobile
+    $('#toggle').on('click touchstart', function(e) {
+        $(this).toggleClass('active');
+        $('#overlay').toggleClass('open');
+
+        e.preventDefault();
+    });
+    $('nav ul li a').on('click touchstart', function(e) {
+        $('#overlay').removeClass('open');
+        $('#toggle').removeClass('active');
+
+        e.preventDefault();
+    });
+
+    // handle links with @href started with '#' only
+    $('a[href^="#"]').bind('click.smoothscroll touchstart',function (e) {
+        e.preventDefault();
+      
+        var target = this.hash,
+        $target = $(target);
+      
+        $('html, body').stop().animate({
+            'scrollTop': $target.offset().top
+        }, 900, 'swing', function () {
+            window.location.hash = target;
+        });
     });
 
     // Active Nav Link
-    $('nav ul li a').click(function() {
-        $('nav ul li a').removeClass('active');
-        $(this).addClass('active');
-    });
+    //     $('nav ul li a').click(function() {
+    //         $(this).removeClass('active');
+    //         $(this).addClass('active');
+    //     });
 });
 
 

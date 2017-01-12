e7bd46f78ea84e13846a10a08445d9e7849b734e
gnarfugh <kmorales076@gmail.com>
2017-01-12 16:30:23 -0600

    api fix

--- js/app.js
+++ js/app.js
@@ -153,14 +153,10 @@ $('a[href*=#]:not([href=#])').on('click touchstart', function() {
 document.addEventListener('DOMContentLoaded', function () {
   if (document.querySelectorAll('#map-canvas').length > 0)
   {
-    if (document.querySelector('html').lang)
-      lang = document.querySelector('html').lang;
-    else
-      lang = 'en';
 
     var js_file = document.createElement('script');
     js_file.type = 'text/javascript';
-    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCnW4d-50jkkMBjvk8_BR-z4wjY-PK8Ujk&callback=initMap' + lang;
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCnW4d-50jkkMBjvk8_BR-z4wjY-PK8Ujk&callback=initMap';
     document.getElementsByTagName('head')[0].appendChild(js_file);
   }
 });

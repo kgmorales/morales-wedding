f6db7ce4e85adb88d45dc8a1824ce686835dc180
gnarfugh <kmorales076@gmail.com>
2017-01-12 16:28:46 -0600

    api fix

--- js/app.js
+++ js/app.js
@@ -150,7 +150,20 @@ $('a[href*=#]:not([href=#])').on('click touchstart', function() {
         }
     });
 });
+document.addEventListener('DOMContentLoaded', function () {
+  if (document.querySelectorAll('#map-canvas').length > 0)
+  {
+    if (document.querySelector('html').lang)
+      lang = document.querySelector('html').lang;
+    else
+      lang = 'en';
 
+    var js_file = document.createElement('script');
+    js_file.type = 'text/javascript';
+    js_file.src = 'https://maps.googleapis.com/maps/api/js?key=AIzaSyCnW4d-50jkkMBjvk8_BR-z4wjY-PK8Ujk&callback=initMap' + lang;
+    document.getElementsByTagName('head')[0].appendChild(js_file);
+  }
+});
 var directionsDisplay;
 var directionsService = new google.maps.DirectionsService();
 var map;
@@ -193,4 +206,4 @@ var calcRoute = function() {
     });
 }
 
-google.maps.event.addDomListener(window, 'load', initialize);
+google.maps.event.addDomListener(window, 'load', initMap);
